7ad7a7025785ba40097459977aed7b8ab425e0de
HouseCham <ramsesramirezvallejo@gmail.com>
2024-04-24 23:21:48 -0600

    Add gRPC setup and client code for customer service

---
+++ api/core/grpc/grpc.go
@@ -0,0 +1,10 @@
+package grpc
+
+import (
+	customerService "github.com/HouseCham/cerebro/api/core/grpc/customerService"
+)
+
+// SetUpGrpcConnections is a function that sets up all the gRPC connections to all microservices
+func SetUpGrpcConnections() {
+	customerService.SetUpCostumerClient()
+}
